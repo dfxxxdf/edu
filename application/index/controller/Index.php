f22af4999dd302ecf323df4d7d2b5039c4d0f6b1
dfxxxdf <137421378@qq.com>
2021-01-29 16:05:42 +0800

    '上传代码'

---
+++ application/index/controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+//        return '我开发的ThinkPHP5.0.10完整版项目内容，努力，加油';
+      return this.view('index');
+    }
+}
